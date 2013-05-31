068855675db9e49bbb64a803ba8c832f1d01c41b
tomykaira <tomykaira@gmail.com>
2013-05-31 16:20:46 +0900

    Split height_map calculator

--- solver.cpp
+++ solver.cpp
@@ -174,6 +174,51 @@ void resize(int w, int h)
   glRotated(-50, 1.0, 0, 0.0);
 }
 
+double *calculate_height_map(int width, int height, cv::Point3_<double> *normal)
+{
+  double *height_map = new double[width*height];
+  double *map_x = new double[width*height];
+  double *map_y = new double[width*height];
+  cv::Point3_<double> n;
+
+  map_x[0] = 0;
+  map_y[0] = 0;
+
+  for (int y = 1; y < height; ++y) {
+    n = normal[y*width] + normal[(y-1)*width];
+    map_y[y*width] = map_y[(y-1)*width] - (n.y/n.z);
+  }
+
+  for (int y = 1; y < height; ++y) {
+    for (int x = 1; x < width; ++x) {
+      cv::Point3_<double> nx;
+      nx = normal[y*width + x] + normal[y*width + x-1];
+      map_y[y*width + x] = map_y[y*width + (x-1)] - (nx.x/nx.z);
+    }
+  }
+
+  for (int x = 1; x < width; ++x) {
+    n = normal[x] + normal[x-1];
+    map_x[x] = map_x[x-1] - (n.x/n.z);
+  }
+
+  for (int x = 1; x < width; ++x) {
+    for (int y = 1; y < height; ++y) {
+      cv::Point3_<double> ny;
+      ny = normal[y*width + x] + normal[(y-1)*width + x];
+      map_x[y*width + x] = map_x[(y-1)*width + x] - (ny.y/ny.z);
+    }
+  }
+
+  for (int i = 0; i < width*height; ++i) {
+    height_map[i] = (map_x[i] + map_y[i]) / 2.0;
+  }
+
+  delete map_x;
+  delete map_y;
+  return height_map;
+}
+
 int main(int argc, char *argv[])
 {
   cv::Matx34d directions;
@@ -210,42 +255,10 @@ int main(int argc, char *argv[])
     }
   }
 
-  double *height_map = new double[width*height];
-  double *map_x = new double[width*height];
-  double *map_y = new double[width*height];
-  cv::Point3_<double> n;
+  double *height_map = calculate_height_map(width, height, normal);
+
   std::ofstream ofs("result.txt");
-  map_x[0] = 0;
-  map_y[0] = 0;
-
-  for (int y = 1; y < height; ++y) {
-    n = normal[y*width] + normal[(y-1)*width];
-    map_y[y*width] = map_y[(y-1)*width] - (n.y/n.z);
-  }
-
-  for (int y = 1; y < height; ++y) {
-    for (int x = 1; x < width; ++x) {
-      cv::Point3_<double> nx;
-      nx = normal[y*width + x] + normal[y*width + x-1];
-      map_y[y*width + x] = map_y[y*width + (x-1)] - (nx.x/nx.z);
-    }
-  }
-
-  for (int x = 1; x < width; ++x) {
-    n = normal[x] + normal[x-1];
-    map_x[x] = map_x[x-1] - (n.x/n.z);
-  }
-
-  for (int x = 1; x < width; ++x) {
-    for (int y = 1; y < height; ++y) {
-      cv::Point3_<double> ny;
-      ny = normal[y*width + x] + normal[(y-1)*width + x];
-      map_x[y*width + x] = map_x[(y-1)*width + x] - (ny.y/ny.z);
-    }
-  }
-
   for (int i = 0; i < height*width; ++i) {
-    height_map[i] = (map_x[i] + map_y[i]) / 2.0;
     ofs << height_map[i] << std::endl;
   }
 
